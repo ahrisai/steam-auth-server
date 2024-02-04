93af867e5697c9dd0828ef238125782d7a12e331
ahrisai <sasaslapik40@gmail.com>
2024-02-04 15:05:53 +0300

    just update prisma schema for server

--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,101 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { Request, Response } from "express";
 
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser';
-import { Request,Response } from "express";
+import passport from "./steam.js";
+import session from "express-session";
 
-import passport from './steam.js'
-import session from 'express-session';
-
-
-import { PrismaClient } from '@prisma/client';
-import { CsGoData, JwtUser } from './queryTypes.js';
-import AuthMiddleware from './middlewares/AuthMiddleware.js';
+import { PrismaClient } from "@prisma/client";
+import { CsGoData, JwtUser } from "./queryTypes.js";
+import AuthMiddleware from "./middlewares/AuthMiddleware.js";
 const app = express();
 
-let csgoData={} ;
-const allowedOrigins = ['http://localhost:5173','https://squadlink.vercel.app'];
-const prisma = new PrismaClient()
+let csgoData = {};
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://squadlink.vercel.app",
+];
+const prisma = new PrismaClient();
 
 app
   .use(
     cors({
       origin: allowedOrigins,
-      credentials:true,
+      credentials: true,
     })
   )
 
   .use(cookieParser())
   .use(AuthMiddleware)
-  .get('/',(req:Request,res:Response)=>{
-      res.send('hello world')
-  })
-  
-  app.use(session({
-    secret: 'bimg',
+  .get("/", (req: Request, res: Response) => {
+    res.send("hello world");
+  });
+
+app.use(
+  session({
+    secret: "bimg",
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true }
-  }));
-  passport.serializeUser((data, done) => {
-    if(data!=='noData') csgoData={...data}
-    
-    done(null, data); 
-  });
-  app.get('/api/auth/steam',
-  passport.authenticate('steam', { failureRedirect: '/' }),
-  
+    cookie: { secure: true },
+  })
+);
+passport.serializeUser((data, done) => {
+  if (data !== "noData") csgoData = { ...data };
+
+  done(null, data);
+});
+app.get(
+  "/api/auth/steam",
+  passport.authenticate("steam", { failureRedirect: "/" }),
+
   async (req, res) => {
+    if (req.body) {
+      if (Object.keys(csgoData).length === 0)
+        return res
+          .cookie("_csData", "noFaceit")
+          .redirect(303, `http://localhost:5173/home`);
 
-    if(req.body){
-      
+      const jwtUser: JwtUser = req.body as JwtUser;
 
-    if(Object.keys(csgoData).length===0) return res.cookie('_csData','noFaceit').redirect(303, `http://localhost:5173/home`);
-    
-      const jwtUser:JwtUser=req.body as JwtUser
-
-      const newCsGoData={
+      const newCsGoData = {
         ...csgoData,
-        userId:jwtUser.id
-      } as CsGoData
+        userId: jwtUser.id,
+      } as CsGoData;
 
-      const checkCSdata=await prisma.csgo_data.findFirst({where:{steamId:newCsGoData.steamId}})
-      if(checkCSdata){
-        return res.cookie('_csData','exist').redirect(303, `http://localhost:5173/home`)
+      const checkCSdata = await prisma.cs2_data.findFirst({
+        where: { steamId: newCsGoData.steamId },
+      });
+      if (checkCSdata) {
+        return res
+          .cookie("_csData", "exist")
+          .redirect(303, `http://localhost:5173/home`);
       }
-      if(newCsGoData.elo===undefined) res.cookie('_csData','noFaceit').redirect(303, `http://localhost:5173/home`);
+      if (newCsGoData.elo === undefined)
+        res
+          .cookie("_csData", "noFaceit")
+          .redirect(303, `http://localhost:5173/home`);
 
-      if(newCsGoData.elo){
-        console.log(newCsGoData)
-        const response = await prisma.csgo_data.create({data:{...newCsGoData}})
-       return  res.cookie('_csData',JSON.stringify(newCsGoData)).redirect(303, `http://localhost:5173/home`);
+      if (newCsGoData.elo) {
+        console.log(newCsGoData);
+        const response = await prisma.cs2_data.create({
+          data: { ...newCsGoData },
+        });
+        return res
+          .cookie("_csData", JSON.stringify(newCsGoData))
+          .redirect(303, `http://localhost:5173/home`);
       }
-      
-
-
     }
-    
-   
-  });
+  }
+);
 
-app.get('/api/auth/steam/return',
-  
+app.get(
+  "/api/auth/steam/return",
+
   async (req, res) => {
-  
-  res.redirect(303, 'http://localhost:5173/home');
-  });
+    res.redirect(303, "http://localhost:5173/home");
+  }
+);
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
